5cd5b97829951ff8f1878fd76660edcc83d05796
Hugh He <plushugh@gmail.com>
2024-02-08 20:58:49 +0800

    feat: authors support, customizations for team

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   images: {
-    domains: ['gravatar.com']
+    domains: ["2.gravatar.com"],
   },
   eslint: {
     // dirs: ['components', 'layouts', 'lib', 'pages']
@@ -8,17 +8,17 @@ module.exports = {
   async headers() {
     return [
       {
-        source: '/:path*{/}?',
+        source: "/:path*{/}?",
         headers: [
           {
-            key: 'Permissions-Policy',
-            value: 'interest-cohort=()'
-          }
-        ]
-      }
-    ]
+            key: "Permissions-Policy",
+            value: "interest-cohort=()",
+          },
+        ],
+      },
+    ];
   },
-  transpilePackages: ['dayjs']
+  transpilePackages: ["dayjs"],
   // webpack: (config, { dev, isServer }) => {
   //   // Replace React with Preact only in client production build
   //   if (!dev && !isServer) {
@@ -30,4 +30,4 @@ module.exports = {
   //   }
   //   return config
   // }
-}
+};
